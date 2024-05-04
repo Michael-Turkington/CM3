50804ef8dbd8538f13e067eda9a71789e5073c53
Michael Turkington <62445676+Michael-Turkington@users.noreply.github.com>
2024-05-04 20:02:27 +0100

    First

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapCameraKit, createMediaStreamSource } from "@snap/camera-kit";
+import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/camera-kit";
 
 async function initializeCamera() {
     const apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzExNjAxNjAyLCJzdWIiOiIyY2ZkMzFiZC1lMWFmLTQ1N2QtYWJmYy1hOThhNzkzMzRlMWZ-U1RBR0lOR34wOTM2YTE4NC00NjJiLTQ5YjgtOGZjYi05YzAwMDNmMmUwNzUifQ.xEpM3aiqXNU6uIM6w70BfsWag42WdXt3wnGv-hckOUk";
@@ -12,19 +12,20 @@ async function initializeCamera() {
     const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
     // Adjust canvas display size based on the device
-    canvas.style.width = isMobile ? '290px' : '1080px';
-    canvas.style.height = isMobile ? '501px' : '607px';
+    canvas.style.width = isMobile ? '250px' : '1080px';
+    canvas.style.height = isMobile ? '445px' : '607px';
+    
 
     const session = await cameraKit.createSession({ liveRenderTarget: canvas });
     session.events.addEventListener('error', (event) => {
         console.log('Lens error:', event.detail.error);
     });
 
-    const facingMode = isMobile ? "environment" : "user";
+    const facingMode = isMobile ? "user" : "user";
     const stream = await navigator.mediaDevices.getUserMedia({
         video: { facingMode: facingMode }
     });
-    const source = createMediaStreamSource(stream);
+    const source = createMediaStreamSource(stream, { transform: Transform2D.MirrorX, cameraType: 'user' });
     await session.setSource(source);
 
     // Adjust render size based on the device
@@ -34,7 +35,7 @@ async function initializeCamera() {
         await source.setRenderSize(1920, 1080);
     }
 
-    const lensId = isMobile ? "1bce51d2-98c9-48d4-bb81-8f935b17fd12" : "50507980875";
+    const lensId = isMobile ? "7b594534-37c6-4909-80af-cf3117f4601e" : "7b594534-37c6-4909-80af-cf3117f4601e";
     const groupId = "663f5bb4-e694-4260-862f-8979394d866a";
 
     const lens = await cameraKit.lensRepository.loadLens(lensId, groupId);
@@ -54,5 +55,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Wait for the DOM to be fully loaded before attaching event listeners
 // Wait for the DOM to be fully loaded before attaching event listeners
